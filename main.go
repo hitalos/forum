9798174817f0141b1a185137e258d8699357a6f4
crgimenes <crg@crg.eti.br>
2023-05-01 10:32:23 -0300

    add handlers

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"embed"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"crg.eti.br/go/config"
@@ -22,11 +24,56 @@ var (
 	assets embed.FS
 )
 
+func getParameters(prefix string, r *http.Request) ([]string, error) {
+	validateSlug := func(s string) bool {
+		if s == "" {
+			return false
+		}
+
+		for _, c := range s {
+			if !((c >= 'a' && c <= 'z') ||
+				(c >= '0' && c <= '9') ||
+				c == '-') {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	path := strings.ReplaceAll(r.URL.Path, "//", "/")
+	path = strings.TrimPrefix(path, prefix)
+	path = strings.TrimSuffix(path, "/")
+
+	if path == "" {
+		return []string{}, nil
+	}
+
+	a := strings.Split(path, "/")
+
+	for i := range a {
+		if !validateSlug(a[i]) {
+			return nil, fmt.Errorf("Invalid slug: %q, %q, path: %q", r.URL.Path, a[i], path)
+		}
+	}
+
+	return a, nil
+}
+
+func redirectHandler(prefix string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, prefix, http.StatusMovedPermanently)
+	}
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 
-	if r.Method == http.MethodPost {
-		r.ParseForm()
-		fmt.Println(r.Form.Get("message"))
+	parameters, err := getParameters("/forum/", r)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Bad Request"))
+		return
 	}
 
 	index, err := assets.ReadFile("assets/index.html")
@@ -45,6 +92,45 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
+	for k, v := range parameters {
+		fmt.Printf("%d: %s\n", k, v)
+	}
+
+	for k, v := range r.URL.Query() {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+
+	if r.Method == http.MethodPost &&
+		r.Header.Get("Content-Type") == "application/x-www-form-urlencoded" {
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for k, v := range r.PostForm {
+			fmt.Printf("%s: %s\n", k, v)
+		}
+	}
+
+	if r.Method == http.MethodPost &&
+		r.Header.Get("Content-Type") == "application/json" {
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Printf("%v\n", string(b))
+	}
+
+	fmt.Printf("RemoteAddr: %s\n", r.RemoteAddr)
+	fmt.Printf("Host: %s\n", r.Host)
+	fmt.Printf("RequestURI: %s\n", r.RequestURI)
+	fmt.Printf("URL: %s\n", r.URL)
+	fmt.Printf("Method: %s\n", r.Method)
+	fmt.Printf("Header: %v\n", r.Header)
+
 }
 
 func main() {
@@ -59,7 +145,8 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/forum", homeHandler)
+	mux.HandleFunc("/forum", redirectHandler("/forum/"))
+	mux.HandleFunc("/forum/", homeHandler)
 
 	s := &http.Server{
 		Handler:        mux,

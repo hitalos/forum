6f9c572cce8a6788b77d04783ae2e8e5a63888b4
crgimenes <crg@crg.eti.br>
2023-05-01 09:35:06 -0300

    clear main

--- main.go
+++ main.go
@@ -2,70 +2,32 @@ package main
 
 import (
 	"embed"
-	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"time"
 
-	"github.com/dghubble/gologin/v2"
-	"github.com/dghubble/gologin/v2/github"
-	"golang.org/x/oauth2"
-	githubOAuth2 "golang.org/x/oauth2/github"
-
 	"crg.eti.br/go/config"
 	_ "crg.eti.br/go/config/ini"
-
-	"crg.eti.br/go/forum/session"
 )
 
 type Config struct {
-	GithubClientID     string `ini:"github_client_id" cfg:"github_client_id" cfgRequired:"true" cfgHelper:"Github Client ID"`
-	GithubClientSecret string `ini:"github_client_secret" cfg:"github_client_secret" cfgRequired:"true" cfgHelper:"Github Client Secret"`
-	GithubCallbackURL  string `ini:"github_callback_url" cfg:"github_callback_url" cfgRequired:"true" cfgHelper:"Github Callback URL"`
-	DatabaseURL        string `ini:"database_url" cfg:"database_url" cfgRequired:"true" cfgHelper:"Database URL"`
-	Port               int    `ini:"port" cfg:"port" cfgDefault:"8080" cfgHelper:"Port"`
-}
-
-type sessionData struct {
-	OAuthProvider string
-	UserName      string
-	AvatarURL     string
-	SessionID     string
+	Port  int    `ini:"port" cfg:"port" cfgDefault:"8080" cfgHelper:"Port"`
+	DBURL string `ini:"dburl" cfg:"dburl" cfgHelper:"Database URL" cfgRequired:"true"`
 }
 
 var (
-	sc *session.Control
-
 	//go:embed assets/*
 	assets embed.FS
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("homeHandler")
 
-	sid, sd, ok := sc.Get(r)
-	if !ok {
-		fmt.Println("1 session not found")
-		sid, sd = sc.Create()
+	if r.Method == http.MethodPost {
+		r.ParseForm()
+		fmt.Println(r.Form.Get("message"))
 	}
-
-	if sd.Data != nil {
-		sdAUX, ok := sd.Data.(*sessionData)
-		if !ok {
-			log.Fatal("type assertion failed sessionData")
-		}
-		fmt.Println("name:", sdAUX.UserName)
-	} else {
-		fmt.Println("sd.Data is nil")
-	}
-
-	// renew session
-	sc.Save(w, sid, sd)
-
-	fmt.Println("sid:", sid)
-	//////////////////////////
 
 	index, err := assets.ReadFile("assets/index.html")
 	if err != nil {
@@ -77,25 +39,7 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	var (
-		sdAUX *sessionData
-	)
-
-	if sd.Data != nil {
-		sdAUX, ok = sd.Data.(*sessionData)
-		if !ok {
-			log.Fatal("type assertion failed sessionData")
-		}
-	}
-	data := struct {
-		SessionData    *sessionData
-		GitHubLoginURL string
-		LogoutURL      string
-	}{
-		SessionData:    sdAUX,
-		GitHubLoginURL: "/forum/github/login",
-		LogoutURL:      "/forum/logout",
-	}
+	data := struct{}{}
 	err = t.Execute(w, data)
 	if err != nil {
 		log.Fatal(err)
@@ -103,139 +47,19 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func logoutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("logoutHandler")
-	sid, sd, ok := sc.Get(r)
-	if !ok {
-		http.Redirect(w, r, "/forum", http.StatusFound)
-		return
-	}
-
-	sd.Data = nil
-
-	// remove session
-	sc.Delete(w, sid)
-
-	http.Redirect(w, r, "/forum", http.StatusFound)
-}
-
-func issueSession() http.Handler {
-	fmt.Println("issueSession")
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		sid, sd, ok := sc.Get(r)
-		if !ok {
-			fmt.Println("2 session not found")
-			sid, sd = sc.Create()
-		}
-
-		ctx := r.Context()
-		githubUser, err := github.UserFromContext(ctx)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Println("sid:", sid)
-		fmt.Println("githubUser id.........:", *githubUser.ID)
-		fmt.Println("githubUser login......:", *githubUser.Login)
-		fmt.Println("githubUser email......:", *githubUser.Email)
-		fmt.Println("githubUser name.......:", *githubUser.Name)
-		fmt.Println("githubUser avatar.....:", *githubUser.AvatarURL)
-		fmt.Println("githubUser url........:", *githubUser.URL)
-		fmt.Println("githubUser html url...:", *githubUser.HTMLURL)
-		fmt.Println("githubUser followers..:", *githubUser.Followers)
-		fmt.Println("githubUser following..:", *githubUser.Following)
-		fmt.Println("githubUser created at.:", *githubUser.CreatedAt)
-
-		sdAUX := sessionData{
-			OAuthProvider: "github",
-			UserName:      *githubUser.Name,
-			AvatarURL:     *githubUser.AvatarURL,
-			SessionID:     sid,
-		}
-		sd.Data = &sdAUX
-
-		fmt.Println("name:", sdAUX.UserName)
-		// renew session
-		sc.Save(w, sid, sd)
-
-		http.Redirect(w, r, "/forum", http.StatusFound)
-	}
-	return http.HandlerFunc(fn)
-}
-
 func main() {
 	cfg := Config{}
 
-	config.File = "config.ini"
+	config.File = "forum.ini"
 	err := config.Parse(&cfg)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	const cookieName = "forum_session"
-	sc = session.New(cookieName)
-
-	go func() {
-		for {
-			time.Sleep(5 * time.Minute)
-			sc.RemoveExpired()
-		}
-	}()
-
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", homeHandler)
-	//mux.HandleFunc("/login", loginHandler)
-	mux.HandleFunc("/logout", logoutHandler)
-
-	oauth2Config := &oauth2.Config{
-		ClientID:     cfg.GithubClientID,
-		ClientSecret: cfg.GithubClientSecret,
-		RedirectURL:  cfg.GithubCallbackURL,
-		Endpoint:     githubOAuth2.Endpoint,
-	}
-
-	// state param cookies require HTTPS by default; disable for localhost development
-	stateConfig := gologin.DebugOnlyCookieConfig
-
-	mux.Handle(
-		"/github/login",
-		github.StateHandler(
-			stateConfig,
-			github.LoginHandler(oauth2Config, nil)))
-	mux.Handle(
-		"/github/callback",
-		github.StateHandler(
-			stateConfig,
-			github.CallbackHandler(oauth2Config, issueSession(), nil)))
-
-	// recebe post de usuário
-	mux.HandleFunc("/forum/post", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("postHandler")
-		sid, sd, ok := sc.Get(r)
-		if !ok {
-			http.Redirect(w, r, "/forum", http.StatusFound)
-			return
-		}
-
-		sdAUX, ok := sd.Data.(*sessionData)
-		if !ok {
-			log.Fatal("type assertion failed sessionData")
-		}
-		fmt.Println("name:", sdAUX.UserName)
-
-		var post string
-		err := json.NewDecoder(r.Body).Decode(&post)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Println("sid:", sid)
-		fmt.Println("post:", post)
-	})
+	mux.HandleFunc("/forum", homeHandler)
 
 	s := &http.Server{
 		Handler:        mux,
@@ -244,8 +68,6 @@ func main() {
 		WriteTimeout:   5 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-
-	println(cfg.DatabaseURL)
 
 	log.Printf("Listening on port %d\n", cfg.Port)
 	log.Fatal(s.ListenAndServe())
